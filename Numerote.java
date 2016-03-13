7b5a29184ca40c725c641cb6b2f59e6f01ff799c
juanmele <juan.mele97@gmail.com>
2016-03-13 16:54:53 -0600

    probando que se actualice

--- Numerote.java
+++ Numerote.java
@@ -1,11 +1,17 @@
 public class Numerote {
+
 		private byte [] numerote;
 		private boolean signo;
+
 		public Numerote(){
+
 			numerote =new byte [1];
 			numerote[0]=0;
+
 		}
+
 		public Numerote(String a){
+
 			numerote= new byte [a.length()];
 			try{
 
@@ -19,16 +25,27 @@ public class Numerote {
 			}
 
 		}
+
 		public Numerote suma(Numerote a){
+
 			return new Numerote("00");
+
 		}
+
 		public Numerote resta(Numerote a){
+
 			return new Numerote("00");
+
 		}
+
 		public Numerote multiplica(Numerote a){
+
 			return new Numerote("00");
+
 		}
+
 		public String toString(){
+
 			StringBuffer palabra= new StringBuffer();
 			for(int i=0;i<this.numerote.length;i++){
 				palabra.append(this.numerote[this.numerote.length-1-i]);
@@ -36,10 +53,14 @@ public class Numerote {
 			}
 
 			return palabra.toString();
+
 		}
+
 		public static void main(String []args){
+
 			Numerote tnum= new Numerote("19990");
 			System.out.println(tnum.toString());
+			
 		}
 
 	}
